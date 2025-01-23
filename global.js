bf784f8779ddbdc23d4472d5c077b685dd456e68
Ria Juneja <rjuneja@ucsd.edu>
2025-01-22 21:25:11 -0800

    step 3

--- global.js
+++ global.js
@@ -3,13 +3,42 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-const navLinks = $$("nav a");
-console.log(navLinks);
+// const navLinks = $$("nav a");
+// console.log(navLinks);
 //asked chatGPT to help me with this step as i was a little lost
-const currentPage = window.location.pathname;
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+  
+// if (currentLink) {
+// // or if (currentLink !== undefined)
+// currentLink.classList.add('current');
+// }
 
-navLinks.forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("current");
-  }
-});
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    // add the rest of your pages here
+];
+
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+    a.target = a.host !== location.host ? '_blank' : '';
+    nav.append(a);
+}
