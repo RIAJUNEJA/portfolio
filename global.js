143ac4ed325190f484f4c60cb3b3530bf9fe7f20
Ria Juneja <rjuneja@ucsd.edu>
2025-01-22 20:10:49 -0800

    class=current

--- global.js
+++ global.js
@@ -3,3 +3,13 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+const navLinks = $$("nav a");
+console.log(navLinks);
+//asked chatGPT to help me with this step as i was a little lost
+const currentPage = window.location.pathname;
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("current");
+  }
+});
