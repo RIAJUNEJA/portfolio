e0c7d3e29021858e1df0fb445dcb54e7a0731424
Ria Juneja <rjuneja@ucsd.edu>
2025-01-29 01:45:02 -0800

    changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,13 @@
+// import { fetchJSON, renderProjects } from '../global.js';
+// const projects = await fetchJSON('../lib/projects.json');
+// const projectsContainer = document.querySelector('.projects');
+// renderProjects(projects, projectsContainer, 'h2');
 import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+
+fetchJSON('../lib/projects.json').then(projects => {
+    const projectsContainer = document.querySelector('.projects');
+
+    projects.forEach(project => {
+        renderProjects(project, projectsContainer, 'h2');
+    });
+});
